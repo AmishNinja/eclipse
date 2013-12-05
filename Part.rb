f137ff90d028e808ceb8abf19b160c67b0d4e0d1
Sean O'Loughlin <Seanuww@gmail.com>
2013-12-05 00:34:22 -0600

    don't ask :P

--- Part.rb
+++ Part.rb
@@ -7,53 +7,36 @@ class Part
 		sync_attrs
 	end
 
-	#delegators 
-	def hp= value
-		ship.hp += value
-	end
-
-	def init= value
-		ship.init += value
-	end
-
-	def hit_bonus= value
-		ship.hit_bonus += value
-	end
-
-	def shield= value
-		ship.shield += value
-	end
-
 	def sync_attrs
 		case @name
 		when "hull"
-			self.hp=1
+			self.ship.hp += 1
 		when "improved hull"
-			self.hp=2
+			self.ship.hp += 2
 		when "shard hull"
-			self.hp=3
+			self.ship.hp += 3
 		when "gauss shield"
-			self.shield=1
+			self.ship.shield += 1
 		when "phase shield"
-			self.shield=2
+			self.ship.shield += 2
 		when "flux shield"
-			self.shield=3
+			self.ship.shield += 3
 		when "electron computer"
-			self.hit_bonus=1
+			self.ship.hit_bonus += 1
 		when "positron computer"
-			self.hit_bonus=2
-			self.init=1
+			self.ship.hit_bonus += 2
+			self.ship.init += 1
 		when "gluon computer"
-			self.hit_bonus=3
-			self.init=2
+			self.ship.hit_bonus += 3
+			self.ship.init += 2
 		when "nuclear drive"
-			self.init=1
+			self.ship.init += 1
 		when "fusion drive"
-			self.init=2
+			self.ship.init += 2
 		when "tachyon drive"
-			self.init=3
+			self.ship.init += 3
 		when "conformal drive"
-			self.init=2
+			self.ship.init += 2
 		end
 		return self
 	end
